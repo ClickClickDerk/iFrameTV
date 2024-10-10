e627953b4f1e67b5565b1b9ddd4869665ef0416a
ClickClickDerk <nickcatalano08@gmail.com>
2024-10-09 20:55:51 -0400

    Create docFlicks.js

---
+++ javascript/docFlicks.js
@@ -0,0 +1,5 @@
+function docsSrc() {
+document.getElementById("racingExtinction").setAttribute("onclick", "play('https://drive.google.com/file/d/0B1XkNy-9gbHKU2JqOXZabHF4Z3M/preview', id)");
+document.getElementById("jiroDreamsOfSushi").setAttribute("onclick", "play('https://drive.google.com/file/d/0B6NlM1Uh_EmzVXZSbjFYQ29YVDg/preview', id)");
+document.getElementById("chasingIce").setAttribute("onclick", "play('https://drive.google.com/file/d/0BwhgMeNGU6QiMzl4U3hFNW1qeEE/preview', id)");
+};
